ba1f8867dd1bf4209482f147f740e9919000d458
cloudwithkomal <komals.1494@gmail.com>
2025-02-26 16:29:40 -0500

    made the changes to author section and changed the name

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const poems = [
   {
-    author: "Cece",
+    author: "Komal",
     poem: "Roses are red \n Violets are blue \n Sunflowers are yellow \n I bet you were expecting something romantic but no -- these are just gardening facts.",
     image: "sunflower.jpeg",
   },
   {
-    author: "Anvit",
+    author: "Lets",
     poem: "Roses are red \n Pizza sauce is too \n I ordered a large \n None of it is for you ",
     image: "pizza.jpeg",
   },
   {
-    author: "Ernie",
+    author: "Go",
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
   },  
