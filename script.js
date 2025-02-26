40b553ef7afb8695ca6c06756839cb938e5417b2
christinz <christinyzhong@gmail.com>
2025-02-26 16:29:25 -0500

    added poem object

--- script.js
+++ script.js
@@ -14,6 +14,11 @@ const poems = [
     poem: "Roses are red \n Violets are blue \n Jerry Seinfeld is considering \n Making Bee Movie 2",
     image: "bee.jpeg",
   },  
+  {
+    author: "Christin",
+    poem: "Roses are red \n Violets are blue \n My dog is my favourite \n But you're OK too",
+    image: "bee.jpeg",
+  }, 
 ];
 
 let displayPoems = (array) => {
